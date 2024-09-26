f880a0d5abcf3b8a67e716f75a34e4017047a65d
Luiz Ribas <enderdemon31@outlook.com>
2024-09-26 20:19:17 -0300

    commitProva2.2

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,16 +1,21 @@
             <footer>
                 <div class="footer">
-                    <div class="footerhr">
-                        <h5 class="h5Footer">Siga nossas redes sociais!</h5>
+                    <div class="upperDiv">
+                        <div class="footerhr">
+                            <h5 class="h5Footer">Siga nossas redes sociais!</h5>
 
-                        <div class="FooterDH">
-                            <img src="img/instaLogo-removebg-preview.png" alt="insta" width="2%" height="40%" class="FooterDHItens">
-                            <img src="img/faceLogo-removebg-preview.png" alt="face" width="2%" height="60%" class="FooterDHItens">
-                            <img src="img/X-Logo-removebg-preview.png" alt="x" width="2%" height="32%" class="FooterDHItens">
+                            <div class="FooterDH">
+                                <img src="img/instaLogo-removebg-preview.png" alt="insta" width="2%" height="40%" class="FooterDHItens">
+                                <img src="img/faceLogo-removebg-preview.png" alt="face" width="2%" height="60%" class="FooterDHItens">
+                                <img src="img/X-Logo-removebg-preview.png" alt="x" width="2%" height="32%" class="FooterDHItens">
+                            </div>
+                        </div>
+                        <div class="footerhl">
+                            
                         </div>
                     </div>
-                    <div class="footerhl">
-                        
+                    <div class="lowerDiv">
+                        <p class="cpright">© 2024 -Todos os direitos reservados</p>
                     </div>
                 </div>
             </footer>
